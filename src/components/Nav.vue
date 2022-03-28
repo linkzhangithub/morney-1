f15a4bf154c4a5ce6edea07c885475dfc47dbed1
linkzhang <1519815799@qq.com>
2022-03-29 02:52:43 +0800

    创建 <Icon />

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,24 +1,39 @@
 <template>
-    <div class="nav">
-      <router-link to="/money">
-      <svg>
-          <use xlink:href="#label"/>
-      </svg>
+  <div class="nav">
+    <router-link to="/money">
+      <Icon name="money"/>
       记账
-      </router-link>
+    </router-link>
     |
-    <router-link to="/labels">标签</router-link>
+    <router-link to="/labels">
+      <Icon name="label"/>
+      标签
+    </router-link>
     |
-    <router-link to="/statistics">统计</router-link>
-    </div>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </div>
 </template>
 
 <script lang="ts">
-import x from '@/assets/icons/label.svg';
-
-    // console.log(x);
+import Icon from './Icon.vue';
+//下面代码将存放了 svg 的 icons 文件夹一次性引入
+let importAll = (requireContext:__WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {
+    importAll(require.context('../assets/icons',true,/\.svg$/));
+} catch (error) {
+    console.log(error);
+}
+//下面代码是将 svg 一个一个引入
+// import x from '@/assets/icons/label.svg';
+// import y from '@/assets/icons/money.svg';
+//     console.log(x);
+//     console.log(y);
 
     export default {
+  components: { Icon },
         name: 'Nav'
     };
 </script>
