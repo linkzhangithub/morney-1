6b6a12c08e90536a71c6705f477109b499a6f6dd
linkzhang <1519815799@qq.com>
2022-04-01 03:51:48 +0800

    添加 createdAt 字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
     <Layout class-prefix="layout">
-      {{record}}
       <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
       <Types :value.sync="record.type"/>
       <Notes @update:value="onUpdateNotes"/>
@@ -16,11 +15,14 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import { Component, Watch} from 'vue-property-decorator';
 
-  type Record = {//在 TS 里声明类型
-    tags: string[]
+  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+  
+  type Record = {//在 TS 里声明类型,也可以写类（构造函数）
+    tags: string[] 
     notes: string
     type: string
     amount: number
+    createdAt?: Date
   }
   
   @Component({
@@ -28,7 +30,7 @@ import { Component, Watch} from 'vue-property-decorator';
     })
   export default class Money extends Vue{
     tags = ['衣', '食', '住', '行', '娱乐'];
-    recordList: Record[] = [];
+    recordList: Record[] = recordList;
     record: Record = {
       tags: [], notes: '', type: '-', amount: 0
     };
@@ -42,9 +44,9 @@ import { Component, Watch} from 'vue-property-decorator';
     }
 
     saveRecord(){
-      const record2 = JSON.parse(JSON.stringify(this.record));//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
+      const record2: Record = JSON.parse(JSON.stringify(this.record));//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
+      record2.createdAt = new Date();
       this.recordList.push(record2);
-      console.log(this.recordList);
     }
 
     @Watch('recordList')
