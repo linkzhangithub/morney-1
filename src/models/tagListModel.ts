4dc710bad642e9eb9f9fb90611d6905be475536a
linkzhang <1519815799@qq.com>
2022-04-01 08:01:35 +0800

    展示和创建标签

---
+++ src/models/tagListModel.ts
@@ -0,0 +1,24 @@
+const localStorageKeyName = 'tagsList'
+type TagListModel = {
+    data: string[];
+    fetch: () => string[];
+    create: (name: string) => 'success' | 'duplicated';//联合类型(类似枚举，不属于七个基本数据类型)
+    save: () => void;
+}
+const tagListModel: TagListModel = {
+    data: [],
+    fetch() {
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+        return this.data;
+    },
+    create(name: string) {
+        if (this.data.indexOf(name) >= 0) { return 'duplicated'; }
+        this.data.push(name);
+        this.save();
+        return 'success';
+    },
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
+    }
+};
+export default tagListModel;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,16 +14,17 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import { Component, Watch} from 'vue-property-decorator';
-  import model from '@/model';
+  import recordListModel from '@/models/recordListModel';
+  import tagListModel from '@/models/tagListModel';
 
-  const recordList = model.fetch();
-  
+  const recordList = recordListModel.fetch();
+  const tagList = tagListModel.fetch();
   
   @Component({
     components: { Tags, Notes, Types, NumberPad }
     })
   export default class Money extends Vue{
-    tags = ['衣', '食', '住', '行', '娱乐'];
+    tags = tagList;
     recordList: RecordItem[] = recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
@@ -38,14 +39,14 @@
     }
 
     saveRecord(){
-      const record2: RecordItem = model.clone(this.record);//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
+      const record2: RecordItem = recordListModel.clone(this.record);//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
       record2.createdAt = new Date();
       this.recordList.push(record2);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      model.save(this.recordList);
+      recordListModel.save(this.recordList);
     }
   };
 </script>
