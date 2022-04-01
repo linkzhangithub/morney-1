f8cbca1d7cc8f7262e27928d19b4c1bf6dc09d20
linkzhang <1519815799@qq.com>
2022-04-01 08:29:18 +0800

    将 tag 的 string 改为 {id, name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,11 @@
 const localStorageKeyName = 'tagsList'
+type Tag = {
+    id: string;
+    name: string;
+}
 type TagListModel = {
-    data: string[];
-    fetch: () => string[];
+    data: Tag[];
+    fetch: () => Tag[];
     create: (name: string) => 'success' | 'duplicated';//联合类型(类似枚举，不属于七个基本数据类型)
     save: () => void;
 }
@@ -12,8 +16,9 @@ const tagListModel: TagListModel = {
         return this.data;
     },
     create(name: string) {
-        if (this.data.indexOf(name) >= 0) { return 'duplicated'; }
-        this.data.push(name);
+        const names = this.data.map(item => item.name);
+        if (names.indexOf(name) >= 0) { return 'duplicated'; }
+        this.data.push({ id: name, name: name });
         this.save();
         return 'success';
     },
