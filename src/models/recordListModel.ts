cc669eb7ea46be35ee7a6db5feb4d87fe494f840
linkzhang <1519815799@qq.com>
2022-04-04 23:44:14 +0800

    将 create 封装到 recordListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,8 +1,12 @@
+import clone from "@/lib/clone";
+
 const localStorageKeyName = 'recordList'
 const recordListModel = {
     data: [] as RecordItem[],
-    clone(data: RecordItem[] | RecordItem) {
-        return JSON.parse(JSON.stringify(data));
+    create(record: RecordItem) {
+        const record2: RecordItem = clone(record);//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
+        record2.createdAt = new Date();
+        this.data.push(record2);
     },
     fetch() {
         this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -44,14 +44,12 @@
     }
 
     saveRecord(){
-      const record2: RecordItem = recordListModel.clone(this.record);//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
-      record2.createdAt = new Date();
-      this.recordList.push(record2);
+      recordListModel.create(this.record);
     }
 
     @Watch('recordList')
     onRecordListChange() {
-      recordListModel.save(this.recordList);
+      recordListModel.save();
     }
   };
 </script>
