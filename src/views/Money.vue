8aa616d44a05f12bd9a2e8f72e9726c68ff4e329
linkzhang <1519815799@qq.com>
2022-04-07 07:39:02 +0800

    Money.vue 使用 Vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,28 +19,29 @@
   import FormItem from '@/components/Money/FormItem.vue';
   import Tags from '@/components/Money/Tags.vue';
   import { Component } from 'vue-property-decorator';
-  import oldStore from '@/store/index2';
   
   @Component({
     components: { Tags, FormItem, Types, NumberPad },
     computed:{
-      count(){
-        return this.$store.state.count;
+      recordList(){
+        return this.$store.state.recordList;
       },
     }
   })
   export default class Money extends Vue{
-    recordList = oldStore.recordList;
     record: RecordItem = {
       tags: [], notes: '', type: '-', amount: 0
     };
-    
+    created(){
+      this.$store.commit('fetchRecords')
+    }
+
     onUpdateNotes(value: string) {
       this.record.notes = value;
     }
 
     saveRecord(){
-      oldStore.createRecord(this.record);
+      this.$store.commit('createRecord', this.record);
     }
   };
 </script>
