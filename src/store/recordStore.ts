77f9321ea7b9e71fed5a8f8c4169bd19f1236619
linkzhang <1519815799@qq.com>
2022-04-06 08:26:14 +0800

    模块化的 store，删除了 models

---
+++ src/store/recordStore.ts
@@ -0,0 +1,24 @@
+import clone from '@/lib/clone';
+
+const localStorageKeyName = 'recordList';
+
+const recordStore = {
+    recordList: [] as RecordItem[],
+    fetchRecords() {
+        this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+        return this.recordList;
+    },
+    saveRecords() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.recordList));
+    },
+    createRecord(record: RecordItem) {
+        const record2: RecordItem = clone(record);//深拷贝：因为 this.record 传给recordList 时地址不发生改变，所以传进去的值个数改变，但会覆盖之前内容；深拷贝是先把值变成字符串，字符串创建一个新对象（与之前对象属性完全一样）
+        record2.createdAt = new Date();
+        this.recordList && this.recordList.push(record2);//可选链语法 recordList?.push
+        recordStore.saveRecords();
+    },
+};
+
+recordStore.fetchRecords();
+
+export default recordStore;
